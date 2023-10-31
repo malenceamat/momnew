9db6d6bd9b0bfc0aeb89707e520aadb71b310867
malenceamat <yatikivan@gmail.com>
2023-10-31 15:37:49 +0300

    сделать видос

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,24 +1,27 @@
 
+
 <div class="sidebar-wrapper sidebar-theme">
     <nav id="sidebar">
 
         <ul class="navbar-nav theme-brand flex-row  text-center">
             <li class="nav-item theme-text">
                 <a href="/admin" class="nav-link"> admin panel? </a>
-
             </li>
 
         </ul>
         <ul class="list-unstyled menu-categories" id="accordionExample">
-            <li class="menu active">
-                <a href="#elements" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle">
+
+            <li class="menu">
+                <a href="#elements" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-zap"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg>
                         <span>Главная страница</span>
                     </div>
-
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                    </div>
                 </a>
-                <ul class="collapse submenu recent-submenu mini-recent-submenu list-unstyled show" id="elements" data-parent="#accordionExample">
+                <ul class="collapse submenu list-unstyled" id="elements" data-parent="#accordionExample">
                     <li>
                         <a href="/slider"> слайдер </a>
                     </li>
@@ -30,7 +33,13 @@
                     </li>
                 </ul>
             </li>
+
+
+
         </ul>
+
     </nav>
+
 </div>
+
 
